b5944d6cb7df47e32262486764553348d66ad664
neo <wfqwfq19920116>
2019-09-24 15:12:21 +0800

    1

--- src/main/java/com/byteDance/spring/controller/TestController.java
+++ src/main/java/com/byteDance/spring/controller/TestController.java
@@ -6,6 +6,9 @@ import com.byteDance.spring.annotation.BDRequestMapping;
 import com.byteDance.spring.annotation.BDRequestParam;
 import com.byteDance.spring.service.TestService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @BDController
 @BDRequestMapping(value = "/test")
 public class TestController {
@@ -16,7 +19,11 @@ public class TestController {
     @BDRequestMapping(value = "/v1")
     public String v1(
             @BDRequestParam(value = "name")
-                    String name) {
+                    String name,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        System.out.println(request);
+        System.out.println(response);
         return testService.getName(name);
     }
 
--- src/main/java/com/byteDance/spring/servlet/BDDispatcherServlet.java
+++ src/main/java/com/byteDance/spring/servlet/BDDispatcherServlet.java
@@ -23,6 +23,7 @@ public class BDDispatcherServlet extends HttpServlet {
     //init-param
     private static final String CONTEXTCONFIGLOCATION = "contextConfigLocation";
 
+    //大写字母
     private final static String[] digits = {
             "A", "B", "C", "D", "E", "F",
             "G", "H", "I", "J", "K", "L",
@@ -42,6 +43,20 @@ public class BDDispatcherServlet extends HttpServlet {
     //handlermapping, String中用的是一个List<Handlermapping>，可能它觉得在map中用key存url冗余了吧
     private static Map<String, Handlermapping> handlermappings = new ConcurrentHashMap<>(16);
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //加载配置文件
+        doLoadingConfigFile(config);
+        //扫描类
+        doClassScanner(properties.getProperty("packageScanner"));
+        //初始化IOC容器
+        initIOC();
+        //@Autowired自动注入
+        doDependancyInjection();
+        //初始化Handlermapping
+        initHandlermapping();
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -51,55 +66,71 @@ public class BDDispatcherServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            //uri
-            String requestURI = req.getRequestURI();
-            //项目路径
-            String servletPath = req.getServletPath();
-            String url = requestURI.replace(servletPath, "");
-            Handlermapping handlermapping = handlermappings.get(url);
-            if (handlermapping == null) {
-                resp.getWriter().write("{code:404,msg:'未找到请求资源'}");
-                return;
-            }
-            Method method = handlermapping.getMethod();
-            Object controller = handlermapping.getController();
-            Map<String, Integer> paramSortMap = handlermapping.getParamSortMap();
-
-            Map<String, String[]> parameterMap = req.getParameterMap();
-            Class<?>[] parameterTypes = handlermapping.getParameterTypes();
-            Object[] args = new Object[parameterTypes.length];
-
-            //请求参数
-            for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-                String key = entry.getKey();
-                if (paramSortMap.containsKey(key)) {
-                    Integer index = paramSortMap.get(key);
-                    Class<?> parameterType = parameterTypes[index];
-                    String value = entry.getValue()[0];
-                    args[index] = converter(parameterType, value);
-                }
-            }
-            //形参是否有HttpServletRequest
-            String servletRequestTypeName = HttpServletRequest.class.getTypeName();
-            if (paramSortMap.containsKey(servletRequestTypeName)) {
-                args[paramSortMap.get(servletRequestTypeName)] = req;
-            }
-            //形参是否有HttpServletResponse
-            String servletResponseTypeName = HttpServletResponse.class.getTypeName();
-            if (paramSortMap.containsKey(servletResponseTypeName)) {
-                args[paramSortMap.get(servletResponseTypeName)] = resp;
-            }
-
-            Object invoke = method.invoke(controller, args);
-            resp.getWriter().write(invoke.toString());
+            //委派给对应的controller
+            doDispatch(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        req.setCharacterEncoding("utf-8");
+        //uri
+        String requestURI = req.getRequestURI();
+        //项目路径
+        String servletPath = req.getServletPath();
+        String url = requestURI.replace(servletPath, "");
+        Handlermapping handlermapping = handlermappings.get(url);
+        if (handlermapping == null) {
+            resp.getWriter().write("{code:404,msg:'未找到请求资源'}");
+            return;
+        }
+        //handlermapping获取对应的方法等
+        Method method = handlermapping.getMethod();
+        Object controller = handlermapping.getController();
+        Map<String, Integer> paramSortMap = handlermapping.getParamSortMap();
+        Class<?>[] parameterTypes = handlermapping.getParameterTypes();
+
+        //request获取参数
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        //方法invoke的实参
+        Object[] args = new Object[parameterTypes.length];
+
+        //请求参数
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String key = entry.getKey();
+            if (paramSortMap.containsKey(key)) {
+                Integer index = paramSortMap.get(key);
+                Class<?> parameterType = parameterTypes[index];
+                String value = entry.getValue()[0];
+                args[index] = converter(parameterType, value);
+            }
+        }
+        //形参是否有HttpServletRequest
+        String servletRequestTypeName = HttpServletRequest.class.getTypeName();
+        if (paramSortMap.containsKey(servletRequestTypeName)) {
+            args[paramSortMap.get(servletRequestTypeName)] = req;
+        }
+        //形参是否有HttpServletResponse
+        String servletResponseTypeName = HttpServletResponse.class.getTypeName();
+        if (paramSortMap.containsKey(servletResponseTypeName)) {
+            args[paramSortMap.get(servletResponseTypeName)] = resp;
+        }
+        //形参是否有......
+
+
+        //执行目标方法
+        Object invoke = method.invoke(controller, args);
+
+        resp.setHeader("Content-type", "text/html;charset=utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write(invoke.toString());
+    }
+
+    //spring有类型转换器，可以自定义类型转换，具体可以看Converter接口
     private Object converter(Class<?> parameterType, String value) {
-        //spring的convert可以自定义，具体可以看Converter接口
         if (parameterType == Integer.class) {
             return Integer.parseInt(value);
         } else if (parameterType == Double.class) {
@@ -109,20 +140,13 @@ public class BDDispatcherServlet extends HttpServlet {
         }
     }
 
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-
-        doLoadingConfigFile(config);
-
-        doClassScanner(properties.getProperty("packageScanner"));
-
-        initIOC();
-
-        doDependancyInjection();
-
-        initHandlermapping();
-
+    //类名首字母转换成小写
+    private String toLowercase(String name) {
+        char[] c = name.toCharArray();
+        if (Arrays.asList(digits).contains(String.valueOf(c[0]))) {
+            c[0] += 32;
+        }
+        return String.valueOf(c);
     }
 
     private void initHandlermapping() {
@@ -163,6 +187,7 @@ public class BDDispatcherServlet extends HttpServlet {
 
             Map<String, Integer> paramSortMap = new HashMap<>(16);
             Class<?>[] parameterTypes = method.getParameterTypes();
+            //method参数上的注解
             Annotation[][] parameterAnnotations = method.getParameterAnnotations();
             for (int i = 0; i < parameterAnnotations.length; i++) {
                 Annotation[] parameterAnnotation = parameterAnnotations[i];
@@ -204,6 +229,7 @@ public class BDDispatcherServlet extends HttpServlet {
                             if (!ioc.containsKey(key)) {
                                 throw new RuntimeException("IOC容器中没有名为：" + key + "的实例");
                             }
+                            field.setAccessible(true);
                             field.set(singleton, ioc.get(key));
                         }
                     }
@@ -250,16 +276,6 @@ public class BDDispatcherServlet extends HttpServlet {
                 e.printStackTrace();
             }
         }
-    }
-
-    //首字母转换成小写
-    private String toLowercase(String name) {
-        char[] c = name.toCharArray();
-        System.out.println(Arrays.toString(c));
-        if (Arrays.asList(digits).contains(String.valueOf(c[0]))) {
-            c[0] += 32;
-        }
-        return String.valueOf(c);
     }
 
     private void doClassScanner(String packageName) {
